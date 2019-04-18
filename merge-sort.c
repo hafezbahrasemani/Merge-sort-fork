80dc236e1dd0bee06f0bc69e2e4eb9ffafa06f61
Hafez Bahrasemani <hafez.bahrasemany@gmail.com>
2019-04-18 13:10:51 +0430

    implementing the sort function which is the known function to sort two parts and merge them

--- merge-sort.c
+++ merge-sort.c
@@ -4,6 +4,34 @@
 
 
 
+/*
+ The sort function which is a recursive function
+ that is based on merge-sort algorithm.
+*/
+void sort(int begin, int end)
+{
+  //The middle point
+  int middle;
+
+  //Base case -> begin >= end, it means sorting is done (:
+  if(begin >= end) return;
+
+  else
+  {
+    middle = (begin + end) / 2;
+
+    //sort the left side of middle
+    sort(begin, middle);
+
+    //sort the right side of middle
+    sort(middle + 1, end);
+
+    //merge two sorted parts
+    merge(begin, middle, middle + 1, end);
+  }
+}
+
+
 int main(int argc, char const *argv[]) {
   /* code */
   return 0;
