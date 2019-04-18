fd0e707921087280c5c633e7d5644f86fb3ecddd
Hafez Bahrasemani <hafez.bahrasemany@gmail.com>
2019-04-18 14:38:36 +0430

    implementing the merge function to merge the two sorted parts

--- merge-sort.c
+++ merge-sort.c
@@ -2,6 +2,22 @@
 #include <sys/shm.h>
 #include <stdio.h>
 
+#define SIZE 10000
+
+
+void sort(int begin, int end);
+void merge(int begin1m int end1, int begin2, int end2);
+
+//Shared memory
+int *shm;
+
+//internal memory
+int internal[SIZE];
+
+
+int main(int argc, char const *argv[]) {
+  
+}
 
 
 /*
@@ -31,8 +47,27 @@ void sort(int begin, int end)
   }
 }
 
+/*
+  merge function that gets 4 integers as the
+  begin and end points of two pats to be merged
+*/
+void merge(int begin1, int end1, int begin2, int end2)
+{
+  int i, j, k, l, m;
+  k = begin1;
 
-int main(int argc, char const *argv[]) {
-  /* code */
-  return 0;
+  //in every iteration compares two ints from both parts and adds the smaller one to the internal array as an temp array
+  for(i = begin1, j = begin2; i <= end1, j <= end2; i++, j++)
+  {
+    if(shm[i] < shm[j]) internal[k++] = shm[i++];
+    else internal[k++] = shm[j++];
+  }
+
+
+  for(l = i; l <= end1, l++) internal[k++] = shm[i];
+
+  for(l = j; l <= end2, l++) internal[k++] = shm[j];
+
+  //filling the actual array
+  for(m = begin1; m <= end2, m++) shm[m] = internal[m];
 }
