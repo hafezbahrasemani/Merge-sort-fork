0f8c8193f6741a773720794ea573d0c978c02361
Hafez Bahrasemani <hafez.bahrasemany@gmail.com>
2019-04-19 12:47:26 +0430

    Removing shmctl from sort function and just using shmdt there, trying to fix the program

--- merge-sort.c
+++ merge-sort.c
@@ -11,92 +11,78 @@
 void sort(int begin, int end);
 void merge(int begin1, int end1, int begin2, int end2);
 void print_array(void);
+void create_shm(void);
 
 //Shared memory
 int *shm;
+int shmid;
+int M;
+key_t key;
 
 //internal memory
 int internal[SIZE];
 
 
 int main(int argc, char const *argv[]) {
-  pid_t left, right;
-  key_t key;
-  int shmid;
-  int M;
   int index = 0;
-  int begin, middle, end;
-  int status1, status2;
+
+  /*
+   * Create the segment.
+   */
+   if ((shmid = shmget(key, sizeof(int) * SIZE, IPC_CREAT | 0666)) < 0) {
+       perror("shmget");
+       exit(1);
+   }
 
    /*
-    * Create the segment.
+    * attach the segment to our data space.
     */
-    if ((shmid = shmget(key, sizeof(int) * SIZE, IPC_CREAT | 0666)) < 0) {
-        perror("shmget");
-        exit(1);
-    }
+   if ((shm = shmat(shmid, NULL, 0)) == (int *) -1) {
+       perror("shmat");
+       exit(1);
+   }
 
-    /*
-     * Now we attach the segment to our data space.
-     */
-    if ((shm = shmat(shmid, NULL, 0)) == (int *) -1) {
-        perror("shmat");
-        exit(1);
-    }
+   //prompt the user to enter M
+   printf("Please enter the value of M: ");
+   scanf("%d", &M);
 
-    //prompt the user to enter M
-    printf("Please enter the value of M: ");
-    scanf("%d", &M);
+   //producing 10000 random numbers
+   while(index < SIZE)
+   {
+     shm[index++] = (rand() % 100000) + 1;
+   }
+   //print_array();
 
-    //producing 10000 random numbers
-    while(index < SIZE)
-    {
-      shm[index++] = (rand() % 100000) + 1;
-    }
-    print_array();
+   sort(0, index - 1);
 
-    //should not use fork() if the number of elements is less than M
-    if(index <= M)
-    {
-      sort(0, index - 1);
-      printf("----Sorted Array------\n");
-      print_array();
+   printf("-----Sorted Array------\n");
+   print_array();
 
-      shmctl(shmid, IPC_RMID, NULL);
-    }
-    else{
-      
-        begin = 0;
-        middle = (index - 1) / 2;
-        end = index - 1;
+//   if((shmdt(shm)) == -1) perror("shmat");
+   shmctl(shmid, IPC_RMID, NULL);
 
-        left = fork();
-        if(left == 0){
-          if((shm = shmat(shmid, (void *) 0, SHM_RND)) == (int *) -1) perror("shmat");
-          sort(begin, middle);
-        }else {
-          right = fork();
-          if(right == 0){
-            if((shm = shmat(shmid, (void *) 0, SHM_RND)) == (int *) -1) perror("shmat");
-            sort(middle + 1, end);
-          } else{
-                waitpid(left, &status1, 0);
-                waitpid(right, &status2, 0);
-
-                merge(begin, middle, middle + 1, end);
-
-                printf("----Sorted Array------\n");
-                print_array();
-
-                if((shmdt(shm)) == -1) perror("shmat");
-                shmctl(shmid, IPC_RMID, NULL);
-          }
-        }
-
-    }
 }
 
 
+void create_shm(void)
+{
+  /*
+   * Create the segment.
+   */
+   if ((shmid = shmget(key, sizeof(int) * SIZE, IPC_CREAT | 0666)) < 0) {
+       perror("shmget");
+       exit(1);
+   }
+
+   /*
+    * Now we attach the segment to our data space.
+    */
+   if ((shm = shmat(shmid, NULL, 0)) == (int *) -1) {
+       perror("shmat");
+       exit(1);
+   }
+
+}
 /*
  The sort function which is a recursive function
  that is based on merge-sort algorithm.
@@ -105,22 +91,54 @@ void sort(int begin, int end)
 {
   //The middle point
   int middle;
+  pid_t left, right;
+  int status1, status2;
 
   //Base case -> begin >= end, it means sorting is done (:
   if(begin >= end) return;
 
   else
   {
-    middle = (begin + end) / 2;
 
-    //sort the left side of middle
-    sort(begin, middle);
+     //should not use fork() if the number of elements is less than M
+     if((end - begin + 1) <= M)
+     {
+       sort(begin, middle);
 
-    //sort the right side of middle
-    sort(middle + 1, end);
+       sort(middle + 1, end);
 
-    //merge two sorted parts
-    merge(begin, middle, middle + 1, end);
+       merge(begin, middle, middle + 1, end);
+
+       //printf("----No Fork()------\n");
+       //print_array();
+
+       //shmctl(shmid, IPC_RMID, NULL);
+     }
+     else{
+         middle = (begin + end) / 2;
+
+         left = fork();
+         if(left == 0){
+           if((shm = shmat(shmid, (void *) 0, SHM_RND)) == (int *) -1) perror("shmat");
+           sort(begin, middle);
+         }else {
+           right = fork();
+           if(right == 0){
+             if((shm = shmat(shmid, (void *) 0, SHM_RND)) == (int *) -1) perror("shmat");
+             sort(middle + 1, end);
+           } else{
+                 waitpid(left, &status1, 0);
+                 waitpid(right, &status2, 0);
+
+                 merge(begin, middle, middle + 1, end);
+
+                 shmdt(shm);
+//                 if((shmdt(shm)) == -1) perror("shmat");
+//                 shmctl(shmid, IPC_RMID, NULL);
+           }
+         }
+
+     }
   }
 }
 
